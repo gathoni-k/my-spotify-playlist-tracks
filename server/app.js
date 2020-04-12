a3c0d8e2d2142a466f9ad13e0cdb8fd5efd49d9c
gathoni-k <maryg.kahiga@gmail.com>
2020-04-12 18:27:54 +0300

    chore: configure to serve index routes
    Initialize passport and serve index routes and use express-session to store
    user info

--- server/app.js
+++ server/app.js
@@ -2,29 +2,42 @@ const express = require('express')
 const path = require('path');
 const logger = require('morgan');
 const http = require('http');
- 
+const cors = require('cors')
+const session = require('express-session')
 const app = express();
+const passport = require('passport')
  
+var indexRouter = require('./routes/index');
+require('./config/passportConfig')
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cors())
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
- 
+app.use(session(
+  {
+      secret: 'my-playlist',
+      resave: true,
+      saveUninitialized: true
+  }
+))
 app.use(express.static(path.join(__dirname, '../client')));
- 
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', indexRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  console.dir(req);
-  console.dir(res);
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   let err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
  
 // error handler
 app.use(function(err, req, res, next) {
@@ -34,7 +47,7 @@ app.use(function(err, req, res, next) {
  
   // render the error page
   res.status(err.status || 500);
-  console.log(error)
+  console.log(err)
 });
  
 // HTTP server
