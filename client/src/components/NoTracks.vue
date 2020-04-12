988d507f3403bc16e83a9dbf115dca59ebc166a9
gathoni-k <maryg.kahiga@gmail.com>
2020-04-12 09:24:38 +0300

    chore: Create NoTracks component
    Rendered when no tracks are in record

---
+++ client/src/components/NoTracks.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <p>To get playlist, login and then click on the get playlist button</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'NoTracks'
+}
+</script>
